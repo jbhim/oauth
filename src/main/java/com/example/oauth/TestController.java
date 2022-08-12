9ab819c5c0254059dfaf182525ce6b5da9f64e38
jint <jint@ywsoftware.com>
2022-08-12 17:55:59 +0800

    Initial commit

---
+++ src/main/java/com/example/oauth/TestController.java
@@ -0,0 +1,38 @@
+package com.example.oauth;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @GetMapping("test")
+    public Map<String, Object> test() {
+        HashMap<String, Object> stringObjectHashMap = new HashMap<>();
+        stringObjectHashMap.put("test", "test");
+        return stringObjectHashMap;
+    }
+
+    @GetMapping("test2")
+    public Map<String, Object> test2() {
+        HashMap<String, Object> stringObjectHashMap = new HashMap<>();
+        stringObjectHashMap.put("test", "test");
+        return stringObjectHashMap;
+    }
+
+
+    @GetMapping("userinfo")
+    public Object userinfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(authentication);
+        return authentication.getPrincipal();
+    }
+
+}
